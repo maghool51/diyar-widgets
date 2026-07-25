ff425109e90143b443abc0642d85eb398dc04bf0
maghool51 <91268503+maghool51@users.noreply.github.com>
2026-07-25 18:39:46 +0330

    Update script.js

--- player/script.js
+++ player/script.js
@@ -40,3 +40,87 @@ function formatTime(sec){
     ":" +
     String(s).padStart(2,"0");
 }
+/* ==========================================
+   Part 2 : Playlist
+========================================== */
+
+fileInput.addEventListener("change", e => {
+
+    files = Array.from(e.target.files);
+
+    playlist.innerHTML = "";
+
+    if(files.length === 0){
+
+        title.textContent = "فایلی انتخاب نشده است";
+        artist.textContent = "Diyar Player";
+
+        return;
+
+    }
+
+    files.forEach((file,index)=>{
+
+        const li = document.createElement("li");
+
+        li.textContent = file.name;
+
+        li.onclick = ()=>{
+
+            loadFile(index);
+
+        };
+
+        playlist.appendChild(li);
+
+    });
+
+    loadFile(0);
+
+});
+
+function loadFile(index){
+
+    if(index<0 || index>=files.length) return;
+
+    currentIndex = index;
+
+    const file = files[index];
+
+    const url = URL.createObjectURL(file);
+
+    title.textContent = file.name;
+
+    artist.textContent = file.type;
+
+    if(file.type.startsWith("video")){
+
+        audio.pause();
+
+        audio.style.display="none";
+
+        video.style.display="block";
+
+        video.src = url;
+
+        player = video;
+
+    }else{
+
+        video.pause();
+
+        video.style.display="none";
+
+        audio.style.display="block";
+
+        audio.src = url;
+
+        player = audio;
+
+    }
+
+    player.play();
+
+    playBtn.textContent="⏸";
+
+}
